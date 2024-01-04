4cb2baefa421fc7012fefd451383be64119888fc
Matheus Diógenes <mathdiogenes@gmail.com>
2024-01-04 17:34:32 -0300

    change: DatabaseSeeder

--- src/main/java/com/nunessports/productssystem/initializer/DatabaseSeeder.java
+++ src/main/java/com/nunessports/productssystem/initializer/DatabaseSeeder.java
@@ -23,37 +23,37 @@ public class DatabaseSeeder implements CommandLineRunner {
   }
 
   private void seedProducts() {
-    Product product = new Product(1L, "Pente", "Pequeno", 5.99d);
+    Product product = new Product(1L, "Maiô de Natação Adulto", "Preto", 54.99d);
     this.productRepository.save(product);
 
-    product = new Product(2L, "Banco", "Grande", 100d);
+    product = new Product(2L, "Camiseta Manga Longa Masculina Com Proteção Solar", "Azul", 68.99);
     this.productRepository.save(product);
 
-    product = new Product(3L, "Ventilador", "Preto", 79.90d);
+    product = new Product(3L, "Sunga Boxer Adulto", "Preto", 53.99d);
     this.productRepository.save(product);
 
-    product = new Product(4L, "Luminaria", "De madeira", 80.00d);
+    product = new Product(4L, "Óculos de Natação", "Adulto", 32.99d);
     this.productRepository.save(product);
 
-    product = new Product(5L, "Sofá", "2 lugares, veludo", 200.00d);
+    product = new Product(5L, "Óculos de Sol", "Redondo", 100.00d);
     this.productRepository.save(product);
 
-    product = new Product(6L, "Notebook gamer", "Preto", 5000.00d);
+    product = new Product(6L, "Kit Raquete Beach Tennis", "Material: madeira", 265.00d);
     this.productRepository.save(product);
 
-    product = new Product(7L, "Cama", "King size", 6000.00d);
+    product = new Product(7L, "Bermuda Masculina", "Bolso Atoalhado", 80.00d);
     this.productRepository.save(product);
 
-    product = new Product(8L, "Travesseiro", "Penas de ganso", 69.90d);
+    product = new Product(8L, "Sunga Slip", "Preta", 69.90d);
     this.productRepository.save(product);
 
-    product = new Product(9L, "Celular", "Branco", 1500.00d);
+    product = new Product(9L, "Sunga Slip", "Vermelha", 69.90d);
     this.productRepository.save(product);
 
-    product = new Product(10L, "Mouse gamer", "Com RGB", 150.00d);
+    product = new Product(10L, "Sunga Slip", "Azul", 69.90d);
     this.productRepository.save(product);
 
-    product = new Product(11L, "Air fryer", "Capacidade: 5 litros", 190.90d);
+    product = new Product(11L, "Bola de futebol", "Dourada", 50.00d);
     this.productRepository.save(product);
 
     product = new Product(12L, "Garrafa", "2 litros", 59.99d);
@@ -68,49 +68,49 @@ public class DatabaseSeeder implements CommandLineRunner {
     product = new Product(15L, "Conjunto de cuecas", "5 unidades", 80.00d);
     this.productRepository.save(product);
 
-    product = new Product(16L, "Revista", "30 páginas", 20.00d);
+    product = new Product(16L, "Bola de futebol", "Verde", 50.00d);
     this.productRepository.save(product);
 
-    product = new Product(17L, "Arroz branco", "1 kg", 2.90d);
+    product = new Product(17L, "Bola de futebol", "Azul", 50.00d);
     this.productRepository.save(product);
 
-    product = new Product(18L, "Feijão preto", "1 kg", 4.00d);
+    product = new Product(18L, "Bola de futebol", "Vermelha", 50.00d);
     this.productRepository.save(product);
 
-    product = new Product(19L, "Feijão branco", "1 kg", 4.00d);
+    product = new Product(19L, "Bola de futebol", "Preta", 50.00d);
     this.productRepository.save(product);
 
-    product = new Product(20L, "Roupão", "Rosa", 50.00d);
+    product = new Product(20L, "Bola de futebol", "Preta", 50.00d);
     this.productRepository.save(product);
 
-    product = new Product(21L, "Roupeiro", "Material: plástico", 30.00d);
+    product = new Product(21L, "Sunga Boxer Adulto", "Amarelo escura", 53.99d);
     this.productRepository.save(product);
 
-    product = new Product(22L, "Perfume", "Amadeirado", 100.00d);
+    product = new Product(22L, "Sunga Boxer Adulto", "Verde escura", 53.99d);
     this.productRepository.save(product);
 
-    product = new Product(23L, "Conjunto de talheres", "Inox", 200.00d);
+    product = new Product(23L, "Sunga Boxer Adulto", "Amarelo clara", 53.99d);
     this.productRepository.save(product);
 
-    product = new Product(24L, "Bola de futebol", "Couro", 150.00d);
+    product = new Product(24L, "Sunga Boxer Adulto", "Verde clara", 53.99d);
     this.productRepository.save(product);
 
-    product = new Product(25L, "Air fryer", "Capacidade: 15 litros", 300.00d);
+    product = new Product(25L, "Sunga Boxer Adulto", "Dourada", 53.99d);
     this.productRepository.save(product);
 
-    product = new Product(26L, "Air fryer", "Capacidade: 30 litros", 400.00d);
+    product = new Product(26L, "Sunga Boxer Adulto", "Cinza", 53.99d);
     this.productRepository.save(product);
 
-    product = new Product(27L, "Frigideira", "Inox", 100.00d);
+    product = new Product(27L, "Sunga Boxer Adulto", "Vermelho escura", 53.99d);
     this.productRepository.save(product);
 
-    product = new Product(28L, "Frigideira", "Teflon", 50.00d);
+    product = new Product(28L, "Sunga Boxer Adulto", "Vermelho clara", 53.99d);
     this.productRepository.save(product);
 
-    product = new Product(29L, "Prato de cerâmica", "Grande", 20.00d);
+    product = new Product(29L, "Sunga Boxer Adulto", "Azul escura", 53.99d);
     this.productRepository.save(product);
 
-    product = new Product(30L, "Prato de vidro", "Grande", 10.00d);
+    product = new Product(30L, "Sunga Boxer Adulto", "Azul clara", 53.99d);
     this.productRepository.save(product);
   }
 }
